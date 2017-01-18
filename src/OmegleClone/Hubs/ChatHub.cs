609a595c1202be4d86ae2e0e70291747dc52ed82
Jace Maniquis <jace.maniquis@gmail.com>
2017-01-19 00:29:27 +0800

    Project Commit

---
+++ src/OmegleClone/Hubs/ChatHub.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Hubs;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OmegleClone.Hubs
+{
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            return Clients.Client(Context.ConnectionId).SetConnectionId(Context.ConnectionId);
+        }
+
+        public void GetConnectionId()
+        {
+            Clients.Caller.getConnectionId(Context.ConnectionId);
+        }
+
+        public void FindChat()
+        {
+            Clients.All.LookForPartner(Context.ConnectionId);
+        }
+
+        public async Task MatchFound(string user1ConnId, string user2ConnId)
+        {
+            var guid = Guid.NewGuid().ToString();
+            await Groups.Add(user1ConnId, guid);
+            await Groups.Add(user2ConnId, guid);
+            Clients.Group(guid).matchFound(guid);
+        }
+
+        public void SendMessage(string roomName, string message)
+        {
+            Clients.Group(roomName).receiveMessage(Context.ConnectionId, message);
+        }
+
+        public async Task DisconnectToRoom(string roomName, string user1ConnId, string user2ConnId)
+        {
+            Clients.Group(roomName).disconnect();
+            await Groups.Remove(user1ConnId, roomName);
+            await Groups.Remove(user2ConnId, roomName);
+        }
+    }
+}
---
+++ src/OmegleClone/Properties/PublishProfiles/Publish-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = '70d0f31f-3399-4663-81d4-65b4dd003a0c'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
